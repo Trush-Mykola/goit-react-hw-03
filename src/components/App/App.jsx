8a81c4b8b8798ee0e7d5d862046c653c92048314
Satanw8you <trusnikolaj41@gmail.com>
2024-03-23 11:56:20 +0200

    finish task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,37 +1,48 @@
 import { useEffect, useState } from "react";
-import Feedback from "../Feedback/Feedback";
-import Options from "../Options/Options";
-import Notification from "../Notification/Notification";
-import Description from "../Description/Description";
+import ContactList from "../ContactList/ContactList";
+import SearchBox from "../SearchBox/SearchBox";
+import { nanoid } from "nanoid";
+import ContactForm from "../ContactForm/ContactForm";
+import css from "./App.module.css";
 
-const reviews = {
-  good: 0,
-  neutral: 0,
-  bad: 0,
-};
+const contactsInfo = [
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+];
 
 const App = () => {
-  const [reviewsValue, setReviewsValue] = useState(() => {
-    const stringifyReviews = localStorage.getItem("reviews");
-    const parsedViews = JSON.parse(stringifyReviews) ?? reviews;
-    return parsedViews;
+  const [contacts, setContacts] = useState(() => {
+    const stringifiedContacts = localStorage.getItem("allContactsData");
+    if (!stringifiedContacts) return contactsInfo;
+    const parsedContacts = JSON.parse(stringifiedContacts);
+    return parsedContacts;
   });
-  const updateFeedback = (feedbackType) => {
-    setReviewsValue({ ...reviewsValue, [feedbackType]: reviewsValue[feedbackType] + 1 });
-  };
-  const resetFeedback = () => {
-    setReviewsValue(reviews);
-  };
+  const [filter, setFilter] = useState("");
+
   useEffect(() => {
-    localStorage.setItem("reviews", JSON.stringify(reviewsValue));
-  }, [reviewsValue]);
-  const totalFeedback = reviewsValue.good + reviewsValue.neutral + reviewsValue.bad;
-  const positiveFeedbackPercentage = Math.round(((reviewsValue.good + reviewsValue.neutral) / totalFeedback) * 100);
+    localStorage.setItem("allContactsData", JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = (contactData) => {
+    const newContact = {
+      ...contactData,
+      id: nanoid(),
+    };
+    setContacts([...contacts, newContact]);
+  };
+  const deleteContact = (contactId) => {
+    setContacts(contacts.filter((contact) => contact.id !== contactId));
+  };
+  const filterContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()) || contact.number.includes(filter));
+
   return (
-    <div>
-      <Description />
-      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} resetFeedback={resetFeedback} />
-      {totalFeedback === 0 ? <Notification /> : <Feedback reviewsValue={reviewsValue} feedbackPercentage={positiveFeedbackPercentage} />}
+    <div className={css.wrapper}>
+      <h1>Phonebook</h1>
+      <ContactForm onAdd={addContact} />
+      <SearchBox value={filter} onFilter={setFilter} />
+      <ContactList contacts={filterContacts} onDelete={deleteContact} />
     </div>
   );
 };
